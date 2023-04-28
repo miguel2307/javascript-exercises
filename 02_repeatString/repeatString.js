4c2c76296d0fdfaa662cd1c7712d5c36afd0c323
Miguel Torres <migueltorres2307@gmail.com>
2023-04-27 18:56:09 -0700

    Completed exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let string = []
+    if (num < 0){
+        return "ERROR";
+    }
+    for (i = 0; i < num; i++){
+        string.push(str);
+    }
+    return string.join("");
 };
 
 // Do not edit below this line
