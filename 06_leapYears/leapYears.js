a83a0f66dacda4b8aa70f7549c9c9cdad99c2d2c
Miguel Torres <migueltorres2307@gmail.com>
2023-06-02 16:48:10 -0700

    Completed exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,18 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (year % 4 === 0 && 
+        year % 100 !== 0)
+        return true;
 
+    else if (year % 400 === 0 && 
+        year % 100 === 0)
+        return true;
+
+    else if (year % 400 === 0 && 
+        year % 100 !== 0)
+        return true;
+
+    else (year % 100 === 0)
+        return false;
 };
 
 // Do not edit below this line
