b5b788407cfb24e3cf8f3e4d7dd55ffa22bb2dfb
Miguel Torres <migueltorres2307@gmail.com>
2023-06-02 14:09:48 -0700

    Completed exercise 5, missing large integer check

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(firstInteger, secondInteger) {
+    if (!Number.isInteger(firstInteger) || !Number.isInteger(secondInteger)) return "ERROR";
+    if (firstInteger < 0 || secondInteger < 0) return "ERROR";
+    if (firstInteger > secondInteger) return "ERROR";
+    let sumIntegers = 0;
+    for (let i = firstInteger; i < secondInteger + 1; i ++) {
+        sumIntegers += i;
+    }
+    return sumIntegers;
 };
 
 // Do not edit below this line
