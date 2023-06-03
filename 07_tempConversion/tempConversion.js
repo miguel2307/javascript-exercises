a075071a231e6f8c01e630befe94abccb03751cf
Miguel Torres <migueltorres2307@gmail.com>
2023-06-02 19:35:28 -0700

    Completed exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celsius = (fahrenheit - 32) * 5 / 9
+  celsius = Math.round(celsius * 10) / 10;
+  return celsius
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheit = (celsius) * (1.8) + 32;
+  fahrenheit = Math.round(fahrenheit * 10) / 10;
+  return fahrenheit
 };
 
 // Do not edit below this line
